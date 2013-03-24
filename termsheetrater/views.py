88ad920db2bd9fcbe7b765fbaa128a220ccf77d5
Vicki <vm2355@columbia.edu>
2013-03-24 02:54:04 -0400

    try again

--- termsheetrater/views.py
+++ termsheetrater/views.py
@@ -10,8 +10,18 @@ from cStringIO import StringIO
 from pyth.plugins.rtf15.reader import Rtf15Reader
 from pyth.plugins.plaintext.writer import PlaintextWriter
 from django.views.decorators.csrf import csrf_exempt
-from pytessosx.pytesser import *
 from subprocess import call
+import os, sys, inspect
+# realpath() with make your script run, even if you symlink it :)
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+	sys.path.insert(0, cmd_folder)
+
+# use this if you want to include modules from a subforder
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"subfolder")))
+if cmd_subfolder not in sys.path:
+	sys.path.insert(0, cmd_subfolder)
+from pytessosx.pytesser import *
 
 def extract_pdf_text(path):
 
